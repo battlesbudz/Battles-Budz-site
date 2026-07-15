f4fc797383a6e45365a8c920549f0e35759c0376
justin <justin.lee.battles@outlook.com>
2026-07-15 00:44:53 -0400

    Create newsletter table on startup

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import express, { type Request, Response, NextFunction } from "express";
 import cookieParser from "cookie-parser";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import { ensureNewsletterSubscribersTable } from "./db";
 
 const app = express();
 app.use(express.json());
@@ -39,6 +40,8 @@ app.use((req, res, next) => {
 });
 
 (async () => {
+  await ensureNewsletterSubscribersTable();
+
   const server = await registerRoutes(app);
 
   const publicRedirects = [
